37f640d7caac819b18371062f5e15d8f9aeb5d81
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-10-26 05:07:42 +0800

    konversi semua halaman ke SSR

--- src/pages/api/tugas.ts
+++ src/pages/api/tugas.ts
@@ -1,8 +1,6 @@
 import { getCollection } from "astro:content";
 import type { APIRoute } from "astro";
 
-export const prerender = false;
-
 export const GET: APIRoute = async ({ url }) => {
   const subject = url.searchParams.get("subject");
   const entries = await getCollection("tugas").then((values) =>
