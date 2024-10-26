e96b19e838a99b7aeca528e864436f2ac03654e7
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-10-26 14:51:43 +0800

    tampilkan toast saat sinkronisasi data ke google drive

--- src/stores.ts
+++ src/stores.ts
@@ -1,9 +1,10 @@
 import type { TaskId } from "$lib/types";
 import { debounce } from "$lib/utils";
 
-import { persisted } from "svelte-persisted-store";
-
 import { get, readonly } from "svelte/store";
+
+import { persisted } from "svelte-persisted-store";
+import { toast } from "svelte-sonner";
 
 const completed = persisted("completed-tasks", new Set<TaskId>(), {
   serializer: {
@@ -13,16 +14,26 @@ const completed = persisted("completed-tasks", new Set<TaskId>(), {
 });
 
 const _syncDataToGoogleDrive = async () => {
-  const response = await fetch("/api/sync-data", {
-    method: "POST",
-    body: JSON.stringify([...get(completed).values()]),
-    headers: { "content-type": "application/json" },
-  });
+  toast.promise(
+    async () => {
+      const response = await fetch("/api/sync-data", {
+        method: "POST",
+        body: JSON.stringify([...get(completed).values()]),
+        headers: { "content-type": "application/json" },
+      });
 
-  if (!response.ok) throw new Error(await response.text());
+      if (!response.ok) throw await response.text();
+    },
+    {
+      duration: 2000,
+      loading: "Sedang sinkronisasi data ke server...",
+      success: "Sinkronisasi selesai!",
+      error: (err) => err as string,
+    }
+  );
 };
 
-const syncDataToGoogleDrive = debounce(_syncDataToGoogleDrive, 5000);
+const syncDataToGoogleDrive = debounce(_syncDataToGoogleDrive, 2000);
 
 export const tasks = {
   completed: readonly(completed),
