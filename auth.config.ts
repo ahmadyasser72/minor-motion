84e7641f472ec918531a53e3d5669e39cbf546c4
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-10-28 10:49:02 +0800

    handle refresh token rotation
    https://authjs.dev/guides/refresh-token-rotation

--- auth.config.ts
+++ auth.config.ts
@@ -6,11 +6,66 @@ export default defineConfig({
     session: ({ session, token }) => ({
       ...session,
       token: token.access_token,
+      valid: token.error === undefined,
     }),
-    jwt: ({ token, account }) => ({
-      access_token: account?.access_token,
-      ...token,
-    }),
+    // https://authjs.dev/guides/refresh-token-rotation#jwt-strategy snippet
+    jwt: async ({ token, account }) => {
+      if (account) {
+        // First-time login, save the `access_token`, its expiry and the `refresh_token`
+        return {
+          ...token,
+          access_token: account.access_token!,
+          expires_at: account.expires_at!,
+          refresh_token: account.refresh_token!,
+        };
+      } else if (Date.now() < token.expires_at * 1000) {
+        // Subsequent logins, but the `access_token` is still valid
+        return token;
+      } else {
+        try {
+          // Subsequent logins, but the `access_token` has expired, try to refresh it
+          if (!token.refresh_token)
+            throw new TypeError("Missing refresh_token");
+
+          // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
+          // at their `/.well-known/openid-configuration` endpoint.
+          // i.e. https://accounts.google.com/.well-known/openid-configuration
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            body: new URLSearchParams({
+              client_id: import.meta.env.GOOGLE_CLIENT_ID,
+              client_secret: import.meta.env.GOOGLE_CLIENT_SECRET,
+              grant_type: "refresh_token",
+              refresh_token: token.refresh_token,
+            }),
+          });
+
+          const tokensOrError = await response.json();
+
+          if (!response.ok) throw tokensOrError;
+
+          const newTokens = tokensOrError as {
+            access_token: string;
+            expires_in: number;
+            refresh_token?: string;
+          };
+
+          token.access_token = newTokens.access_token;
+          token.expires_at = Math.floor(
+            Date.now() / 1000 + newTokens.expires_in
+          );
+          // Some providers only issue refresh tokens once, so preserve if we did not get a new one
+          if (newTokens.refresh_token)
+            token.refresh_token = newTokens.refresh_token;
+          return token;
+        } catch (error) {
+          console.error("Error refreshing access_token", error);
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          token.error = "RefreshTokenError";
+          return token;
+        }
+      }
+    },
   },
   providers: [
     Google({
--- src/override.d.ts
+++ src/override.d.ts
@@ -1,7 +1,18 @@
 import type { DefaultSession } from "@auth/core/types";
+import type { DefaultJWT } from "@auth/core/jwt";
 
 declare module "@auth/core/types" {
   interface Session extends DefaultSession {
     token: string;
+    valid: boolean;
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT extends DefaultJWT {
+    access_token: string;
+    expires_at: number;
+    refresh_token?: string;
+    error?: "RefreshTokenError";
   }
 }
