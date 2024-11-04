6f6a3bb0754bf76d6f5f030833cd2f3f39c5927c
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-04 21:13:01 +0800

    tambah dropdown menu list tugas pada badge jumlah tugas

--- src/components/card-jadwal-content.svelte
+++ src/components/card-jadwal-content.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import ClientOnly from "./client-only.svelte";
 
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import { Badge } from "$lib/components/ui/badge";
+  import { ScrollArea } from "$lib/components/ui/scroll-area";
   import { tasks } from "$lib/stores";
-  import type { ListJadwal, ListStatusTugas } from "$lib/types";
+  import type { ListJadwal, ListStatusTugas, TugasId } from "$lib/types";
 
   import {
     CircleAlert,
+    type Icon as IconType,
     Info,
     LoaderCircle,
     SquareArrowOutUpRight,
@@ -23,15 +26,15 @@
   const formatTime = ({ hour, minute }: typeof jadwal.start) =>
     `${pad(hour)}:${pad(minute)}`;
 
-  const jumlahTugas = $derived.by(() => {
-    let normal = 0;
-    let lewat = 0;
+  const tugas = $derived.by(() => {
+    const normal: TugasId[] = [];
+    const lewat: TugasId[] = [];
 
     for (const [id, status] of allStatusTugas) {
       if ($tasks.isDone(id)) continue;
 
-      if (status === "normal") normal += 1;
-      else if (status === "lewat") lewat += 1;
+      if (status === "normal") normal.push(id);
+      else if (status === "lewat") lewat.push(id);
     }
 
     return { normal, lewat };
@@ -48,19 +51,49 @@
     <SquareArrowOutUpRight class="ml-1 w-4 h-4" />
   </Badge>
 
+  {#snippet tugasBadgeDropdown(
+    tugas: TugasId[],
+    badgeClass: string,
+    Icon: typeof IconType
+  )}
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        {#snippet child({ props })}
+          <Badge class={badgeClass} {...props}>
+            <Icon class="mr-1 w-4 h-4" />
+            {tugas.length}
+          </Badge>
+        {/snippet}
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content class="flex max-h-48">
+        <ScrollArea class="flex-1">
+          {#each tugas as id}
+            <a href="/tugas/{id}">
+              <DropdownMenu.Item class="flex flex-1 cursor-pointer">
+                {id}
+              </DropdownMenu.Item>
+            </a>
+          {/each}
+        </ScrollArea>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  {/snippet}
+
   <ClientOnly>
-    {#if jumlahTugas.normal > 0}
-      <Badge class="bg-indigo-600 hover:bg-indigo-800">
-        <Info class="mr-1 w-4 h-4" />
-        {jumlahTugas.normal}
-      </Badge>
+    {#if tugas.normal.length > 0}
+      {@render tugasBadgeDropdown(
+        tugas.normal,
+        "bg-indigo-600 hover:bg-indigo-800",
+        Info
+      )}
     {/if}
 
-    {#if jumlahTugas.lewat > 0}
-      <Badge class="bg-red-600 hover:bg-red-800">
-        <CircleAlert class="mr-1 w-4 h-4" />
-        {jumlahTugas.lewat}
-      </Badge>
+    {#if tugas.lewat.length > 0}
+      {@render tugasBadgeDropdown(
+        tugas.lewat,
+        "bg-red-600 hover:bg-red-800",
+        CircleAlert
+      )}
     {/if}
 
     {#snippet fallback()}
