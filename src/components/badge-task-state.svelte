356e732bbb2c840086f8338a5c2ecc2854a938e6
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-11-06 16:49:26 +0800

    buat semua badge status tugas menampilkan icon loading sebelum terload

--- src/components/badge-task-state.svelte
+++ src/components/badge-task-state.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import ClientOnly from "./client-only.svelte";
+
   import { Badge } from "$lib/components/ui/badge";
   import type { TugasState } from "$lib/types";
 
-  import { Ban, TrendingDown, TrendingUp } from "lucide-svelte";
+  import { Ban, LoaderCircle, TrendingDown, TrendingUp } from "lucide-svelte";
 
   interface Props {
     state: TugasState;
@@ -13,19 +15,27 @@
   const className = "w-20 uppercase";
 </script>
 
-{#if state === "sudah"}
-  <Badge class="bg-indigo-600 hover:bg-indigo-800 {className}">
-    <TrendingUp class="w-4 h-4 mr-1" />
-    done
-  </Badge>
-{:else if state === "belum"}
-  <Badge class="bg-orange-500 hover:bg-orange-700 {className}">
-    <TrendingDown class="w-4 h-4 mr-1" />
-    belum
-  </Badge>
-{:else if state === "telat"}
-  <Badge class="bg-red-700 hover:bg-red-900 {className}">
-    <Ban class="w-4 h-4 mr-1" />
-    telat
-  </Badge>
-{/if}
+<ClientOnly>
+  {#if state === "sudah"}
+    <Badge class="bg-indigo-600 hover:bg-indigo-800 {className}">
+      <TrendingUp class="w-4 h-4 mr-1" />
+      done
+    </Badge>
+  {:else if state === "belum"}
+    <Badge class="bg-orange-500 hover:bg-orange-700 {className}">
+      <TrendingDown class="w-4 h-4 mr-1" />
+      belum
+    </Badge>
+  {:else if state === "telat"}
+    <Badge class="bg-red-700 hover:bg-red-900 {className}">
+      <Ban class="w-4 h-4 mr-1" />
+      telat
+    </Badge>
+  {/if}
+
+  {#snippet fallback()}
+    <span class="inline-flex justify-center py-0.5 {className}">
+      <LoaderCircle class="w-4 h-4 animate-spin" />
+    </span>
+  {/snippet}
+</ClientOnly>
